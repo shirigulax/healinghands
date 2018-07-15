2f49b16217f3f597d22aafbd7c18ec4e8781d7f7
shirigulax <shirigulax@gmail.com>
2018-07-15 17:11:01 -0600

    Get a basic version with a running frontend

---
+++ src/main/js/app.js
@@ -0,0 +1,21 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+);
