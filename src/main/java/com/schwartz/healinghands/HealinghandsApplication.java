ca280423f061e5f90c119d7b5fb772dff94c1416
shirigulax <shirigulax@gmail.com>
2018-07-15 16:15:53 -0600

    Initial commit

---
+++ src/main/java/com/schwartz/healinghands/HealinghandsApplication.java
@@ -0,0 +1,12 @@
+package com.schwartz.healinghands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealinghandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealinghandsApplication.class, args);
+	}
+}
